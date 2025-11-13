2b02e86af32f0f386f1c90f9b664b2d7021716f0
isura <isura3147@gmail.com>
2025-11-13 09:12:00 +0530

    add component scan to AppConfig.java

--- src/main/java/edu/icet/config/AppConfig.java
+++ src/main/java/edu/icet/config/AppConfig.java
@@ -1,6 +1,7 @@
 package edu.icet.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -17,6 +18,7 @@ import java.util.Properties;
 @Configuration
 @EnableJpaRepositories(basePackages = "edu.icet.repository")
 @EnableTransactionManagement
+@ComponentScan("edu.icet")
 public class AppConfig {
 
     /**
