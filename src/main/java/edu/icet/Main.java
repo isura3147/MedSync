a16f9dafb5ad5aaad0d06a672c9b969091bac144
isura <isura3147@gmail.com>
2025-11-13 09:22:39 +0530

    update Main, Starter to hold spring context

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -1,7 +1,15 @@
 package edu.icet;
 
+import edu.icet.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
-        Starter.main(args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Starter.springContext = context;
+
+        Starter.main(args); //
     }
 }
--- src/main/java/edu/icet/Starter.java
+++ src/main/java/edu/icet/Starter.java
@@ -1,19 +1,29 @@
 package edu.icet;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
 
 public class Starter extends Application {
 
+    public static AnnotationConfigApplicationContext springContext;
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Label label = new Label("Hello, MedSync!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 640, 480);
-        stage.setTitle("Pharmacy Inventory System");
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+
+        fxmlLoader.setControllerFactory(springContext::getBean);
+
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("MedSync - Login");
         stage.setScene(scene);
         stage.show();
     }
